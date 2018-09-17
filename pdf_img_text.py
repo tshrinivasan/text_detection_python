fbac741148c7d9ab53c122c05802ee1226bf9fa7
shrinivasan <shrinivasan@netcalyx.co>
2018-09-17 16:23:14 +0530

    improving

--- pdf_img_text.py
+++ pdf_img_text.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
 Created on Wed Jul 25 11:38:19 2018
 
@@ -17,6 +18,8 @@ To run the example, install the necessary libraries by running:
 
 import argparse
 import os
+import sys
+import time
 
 from utils import Service, encode_image
 from PyPDF2 import PdfFileWriter, PdfFileReader
@@ -27,8 +30,9 @@ def main(photo_file):
     """Run a text detection request on a single image"""
 
     access_token = os.environ.get('VISION_API')
+    print(access_token)
     if access_token == 'None':
-        print "Import VISION API KEY"
+        print("Import VISION API KEY")
     service = Service('vision', 'v1', access_token=access_token)
     with open(photo_file, 'rb') as image:
         base64_image = encode_image(image)
@@ -45,17 +49,21 @@ def main(photo_file):
             }]
         }
         response = service.execute(body=body)
-        text = response['responses'][0]['textAnnotations'][0]['description']
-        print('Found text: {}'.format(text))
+        print(response)
+        if response['responses'][0]:
+            text = response['responses'][0]['textAnnotations'][0]['description']
+            print('Found text: {}'.format(text))
+        else:
+            text = " "
         file1=open("./text/pdf_to_text.txt","a")
         #file1.write(text,'\n')
         file1.write("{}\n".format(text))
         file1.close()
-        print "Text File modified"
+        print("Text File modified")
 
 #Getting Input from user
 
-doc_path=raw_input("Enter the file path with file name : ")
+doc_path=input("Enter the file path with file name : ")
 
 inputpdf = PdfFileReader(open(doc_path, "rb"))
 
@@ -64,14 +72,21 @@ for i in range(inputpdf.numPages):
     output.addPage(inputpdf.getPage(i))
     with open("./pdf/document-page%s.pdf" % i, "wb") as outputStream:
         output.write(outputStream)
-        print "Page %s saved" % i
+        print("Page %s saved" % i)
         # Converting pages into JPG
-    with Image(filename="./pdf/document-page%s.pdf" % i) as img:
-        img.compression_quality = -500
-        img.background_color = Color("white")
-        img.alpha_channel = 'remove'
-        img.save(filename="./image/pdf_image%s.jpg" % i)
-        print "Image %s saved" % i
-        image_file = "./image/pdf_image%s.jpg" %i
+#    with Image(filename="./pdf/document-page%s.pdf" % i) as img:
+#        img.compression_quality = 500
+#        img.background_color = Color("white")
+#        img.alpha_channel = 'remove'
+#        img.save(filename="./image/pdf_image%s.jpg" % i)
+#        print("Image %s saved" % i)
+#        image_file = "./image/pdf_image%s.jpg" %i
         #print image_file
+        time.sleep(3)
+        repair_pdf = "gs -o "  + "./pdf/document-page%s.pdf" % i  + " -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress " + "./pdf/document-page%s.pdf" % i
+        os.system(repair_pdf)
+        pdf_to_jpg = "gs -q -DNOPAUSE -DBATCH -r400 -SDEVICE=jpeg  -sOutputFile=" + "./image/document-page%s.jpg" % i  + " " + "./pdf/document-page%s.pdf" % i
+        print(pdf_to_jpg)
+        os.system(pdf_to_jpg)
+        image_file = "./image/document-page%s.jpg" % i
         main(image_file)
